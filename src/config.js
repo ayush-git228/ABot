a55da436d5c786e24482beb2d23ed8708a88b5f2
ayush-git228 <66081505+ayush-git228@users.noreply.github.com>
2020-10-15 01:37:16 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 module.exports= {
-    "DISCORD_TOKEN" : "NzY1OTAwNjU5ODc3ODcxNjQ2.X4bikw.d5ZY5dFcgekb6wVjPUebHaU0jc8"
+    "DISCORD_TOKEN" :"3 parts Token Get from https://discord.com/developers/applications"
 }
